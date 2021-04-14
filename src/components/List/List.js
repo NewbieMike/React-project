3355d7d1fdf6991963026117e1fc43e9a75b0fb3
Michał Micherda <mmicherdakr@gmail.com>
2021-04-14 13:40:33 +0200

    start project

---
+++ src/components/List/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './list.scss';
+import Hero from '../Hero/Hero';
+class List extends React.Component {
+  render() {
+    return (
+      <section className={styles.component}>
+          <Hero />
+      </section>
+    )
+  }
+}
+
+export default List;
